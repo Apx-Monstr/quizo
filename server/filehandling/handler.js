95ffc9af0f2b525df674b00b84ae4441c9ca15fc
Apx-Monstr <apxmonstr@gmail.com>
2025-02-18 18:34:10 +0530

    Data Folder Replaced

--- server/filehandling/handler.js
+++ server/filehandling/handler.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.writeFile = exports.readFile = void 0;
 const fs_1 = __importDefault(require("fs"));
-const readFile = (file) => JSON.parse(fs_1.default.readFileSync(`./src/data/${file}`, 'utf-8'));
+const readFile = (file) => JSON.parse(fs_1.default.readFileSync(`./data/${file}`, 'utf-8'));
 exports.readFile = readFile;
-const writeFile = (file, data) => fs_1.default.writeFileSync(`./src/data/${file}`, JSON.stringify(data, null, 2));
+const writeFile = (file, data) => fs_1.default.writeFileSync(`./data/${file}`, JSON.stringify(data, null, 2));
 exports.writeFile = writeFile;
--- src/filehandling/handler.ts
+++ src/filehandling/handler.ts
@@ -1,4 +1,4 @@
 import fs from 'fs';
 
-export const readFile = (file: string): any[] => JSON.parse(fs.readFileSync(`./src/data/${file}`, 'utf-8'));
-export const writeFile = (file: string, data: any): void => fs.writeFileSync(`./src/data/${file}`, JSON.stringify(data, null, 2));
+export const readFile = (file: string): any[] => JSON.parse(fs.readFileSync(`./data/${file}`, 'utf-8'));
+export const writeFile = (file: string, data: any): void => fs.writeFileSync(`./data/${file}`, JSON.stringify(data, null, 2));
